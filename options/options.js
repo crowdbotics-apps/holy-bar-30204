f0b07612fee163da0b79cc06acb2ec32c2980667
Crowdbotics <team@crowdbotics.com>
2021-08-30 19:27:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "holy_bar_30204Identifier",
+  url: "https://holy_bar_30204Identifier.botics.co",
+  api: "https://holy_bar_30204Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
